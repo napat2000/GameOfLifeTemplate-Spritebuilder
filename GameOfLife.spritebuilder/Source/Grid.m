c03e435103f9ccf465ddad57e423e6a100d2aa70
Napat Boonsaeng <napat2000@gmail.com>
2014-07-01 22:32:36 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -129,6 +129,8 @@ static const int GRID_COLUMNS = 10;
 
 -(void)updateCreatures
 {
+    int numAlive = 0;
+    
     for(int i=0; i<[_gridArray count]; i++)
     {
         for(int j=0; j<[_gridArray[i] count]; j++)
@@ -137,12 +139,14 @@ static const int GRID_COLUMNS = 10;
             if(currentCreature.livingNeighbors == 3)
             {
                 currentCreature.isAlive = YES;
+                numAlive++;
             } else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >=4)
             {
                 currentCreature.isAlive = NO;
             }
         }
     }
+    _totalAlive = numAlive;
 }
 
 @end
